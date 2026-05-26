7a26443e8227f513d0967c01e5c1246842673040
msebastiansn-oss <msebastian.sn@gmail.com>
2026-05-26 09:12:06 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# CryptoRadar 📈
+App de criptomonedas. PWA instalable sin App Store.
+**URL:** https://msebastiansn-oss.github.io/cryptoradar
---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'cryptoradar-v2';
+const ASSETS = [
+  '/cryptoradar/',
+  '/cryptoradar/index.html',
+  '/cryptoradar/icon-192.png',
+  '/cryptoradar/icon-512.png',
+  '/cryptoradar/manifest.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.pathname.includes('/data/')) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
